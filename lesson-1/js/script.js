e99621ef1ea66100fc76c5e4966906b39ce969d9
Waseem Saidoun <wasaidoun@gmail.com>
2025-05-06 11:31:31 -0400

    Added logic to my updateName() function, changes the em element to your name.

--- lesson-1/js/script.js
+++ lesson-1/js/script.js
@@ -27,7 +27,7 @@ const element = document.querySelector('em');
 
 
 // STEP 3: Assign a click event listener to the above element, and include a reference to the function to be invoked
-element.addEventListener('click', updateName);
+// element.addEventListener('click', updateName);
 
 // STEP 4: Creat a function that waits for the above click event
 function updateName() {
@@ -35,8 +35,9 @@ function updateName() {
 
 
 	// STEP 4a: Display a prompt asking for input, and assign the input to a variable using let
-	
+	let firstName = prompt('Please enter your first name: ');
 	// STEP 4b: Grab the input from the variable and build a new string, then change the text inside the EM element, using the textContent property
+	element.textContent = firstName;
 };
 
 // STEP 5: Comment out the addEventListener above, and replace it with an onclick attribute typed directly into the EM element in the HTML - note that this is common, but it is considered to be bad practice - separate your HTML and your JS
